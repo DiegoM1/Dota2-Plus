30a78e4f4033f9ff17f7a0c7f61e665dcf80a567
diegoM1 <diegomonteagudo@gmail.com>
2023-04-04 13:03:00 -0500

    Added SwiftLint with fastlane

---
+++ Pods/Target Support Files/Pods-Dota2 plus-Dota2 plusUITests/Pods-Dota2 plus-Dota2 plusUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dota2_plus_Dota2_plusUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Dota2_plus_Dota2_plusUITests
+@end
---
+++ Pods/Target Support Files/Pods-Dota2 plus/Pods-Dota2 plus-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dota2_plus : NSObject
+@end
+@implementation PodsDummy_Pods_Dota2_plus
+@end
